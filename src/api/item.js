43aca34985875c4f14ab9cdc0cdecc3a851dd524
nee <nee@hoge.cn>
2018-07-15 22:23:16 +0800

    调整首页样式以及详情增加相关视频推荐

--- src/api/item.js
+++ src/api/item.js
@@ -25,8 +25,28 @@ export function getItems(url) {
   })
 }
 
-export function getItem(item_id) {
-  let url = 'https://baobab.kaiyanapp.com/api/v1/video/' + item_id
+export function getRelatedItems(itemId) {
+  let url = 'https://baobab.kaiyanapp.com/api/v4/video/related?id=' + itemId
+
+  return wepy.request(url).then((res) => {
+    var statusCode = res.statusCode
+    var data = res.data
+    if (statusCode === 200) {
+      var items = data.itemList
+        .filter(item => item.type === 'videoSmallCard')
+        .map(item => item.data)
+      return Promise.resolve(items)
+    } else {
+      return Promise.reject({
+        'error_code': statusCode + '',
+        'error_message': '服务器出错了...'
+      })
+    }
+  })
+}
+
+export function getItem(itemId) {
+  let url = 'https://baobab.kaiyanapp.com/api/v1/video/' + itemId
   return wepy.request(url).then((res) => {
     var statusCode = res.statusCode
     var data = res.data
